f49745f20e4a4f01bec192a81032f12a40b9fa5f
Karthik <dkkarthik2000@gmail.com>
2021-10-06 11:28:27 +0530

    ontime submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 /*Fill your code */
+
+function renderall()
+{
+
+}
+
+
+let buttons=document.querySelectorAll("#buttonslist button")
+let layers= document.querySelectorAll(".cakelayer")
+
+buttons[0].addEventListener("click",chocolatelayer)
+function chocolatelayer()
+{
+    layers[0].classList.toggle("#lastlayer")
+
+}
